3969c45316e66ffbc11111bf8786d33b0f7e53d3
Pablo Garcia <pablogarcia440@gmail.com>
2019-05-02 13:29:08 -0300

    Criação da rota principal

--- config/route.js
+++ config/route.js
@@ -1,12 +1,23 @@
 const route = require('express').Router();
 
 route.use(function timeLog(req, res, next) {
-    var antes = Date.now();
-    next();
-    var duracao = Date.now() - antes;
-    console.log(`Timer: ${duracao}ms`);
+  var antes = Date.now();
+  next();
+  var duracao = Date.now() - antes;
+  console.log(`Timer: ${duracao}ms`);
+});
+
+route.get('/', SearchForInformationByCompany);
+
+module.exports = route;
+
+
+function SearchForInformationByCompany(req, res) {
+  let body = req.body.companyName;
+  res.json({
+    msg: "Teste de rota",
+    body: body,
+    info: "verificando a passagem de parâmetro"
   });
-
-  // Criar rotas - get, post
-
-  module.exports = route;
+  console.log(body);
+}

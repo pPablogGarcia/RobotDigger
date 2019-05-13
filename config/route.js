ab328162dd2db2e02708e08c68cbf8066944039a
Pablo Garcia <pablogarcia440@gmail.com>
2019-05-13 16:10:58 -0300

    Teste de deploy

--- config/route.js
+++ config/route.js
@@ -2,9 +2,16 @@ const route = require('express').Router();
 const searchController = require('../controller/searchController.js');
 
 route.post('/', SearchForInformationByCompany);
+route.get('/', (req, res) => {
+  res.status(200).json({
+    msg: "Teste de Rota!",
+    author: "Pablo Garcia"
+  });
+});
 
 async function SearchForInformationByCompany(req, res) {
   let body = req.body.companyName;
+  console.log(body);
   res.json(await searchController.search(body));
 };
 
